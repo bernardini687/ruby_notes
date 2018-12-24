1bafd41c3adf65d66e2723ee56625509308ec828
Oscar Bernardini <bernardini687@gmail.com>
2018-12-24 13:55:46 +0100

    Update part-18

--- part-18/classes/gadget.rb
+++ part-18/classes/gadget.rb
@@ -1,17 +1,23 @@
 # Generally you want one file per class and filename == ClassName
 
 class Gadget
-  def initialize
+  def initialize # This is a private method
     @user = "User #{rand(1..100)}"
     @key = "****"
     @serial = "#{("a".."z").to_a.sample}-#{rand(100..999)}"
   end
+
+  def info # This is a public instance method
+    "Gadget #{@serial} has the username: #{@user}."
+  end
 end
 
 phone = Gadget.new
-p phone
-#<Gadget:0x007f9eee969580 @user="User 16", @key="****", @serial="o-513">
-
 laptop = Gadget.new
-p laptop
-#<Gadget:0x007f9eee968bf8 @user="User 50", @key="****", @serial="v-244">
+
+puts phone.info  # => Gadget h-632 has the username: User 37.
+puts laptop.info # => Gadget v-278 has the username: User 48.
+
+# .info is a method applicable to objects made from the gadget class
+
+p phone.methods - Object.methods # => [:info]
