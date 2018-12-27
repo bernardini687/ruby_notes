7ea0e71f41675df683b2e19bb91093fda6c045a1
Oscar Bernardini <bernardini687@gmail.com>
2018-12-27 12:58:24 +0100

    Update part-18

--- part-18/classes/gadget.rb
+++ part-18/classes/gadget.rb
@@ -7,17 +7,44 @@ class Gadget
     @serial = "#{("a".."z").to_a.sample}-#{rand(100..999)}"
   end
 
+  def user
+    @user
+  end
+
+  def serial
+    @serial
+  end
+
   def info # This is a public instance method
-    "Gadget #{@serial} has the username: #{@user}."
+    "Gadget #{@serial} has the username: #{@user}.\nYes, it's a #{self.class} of ID #{self.object_id}"
+    # self will refer to the object on which .info will be called upon
+  end
+
+  def to_s # This ovverides the default .to_s
+    "Miao"
   end
 end
 
 phone = Gadget.new
 laptop = Gadget.new
 
-puts phone.info  # => Gadget h-632 has the username: User 37.
-puts laptop.info # => Gadget v-278 has the username: User 48.
+puts phone.info
+# => Gadget h-632 has the username: User 37.
+#    Yes, it's a Gadget of ID 70282518413860
+
+puts laptop.info
+# => Gadget v-278 has the username: User 48.
+#    Yes, it's a Gadget of ID 70347467202080
 
 # .info is a method applicable to objects made from the gadget class
 
 p phone.methods - Object.methods # => [:info]
+
+# By default, .to_s is a method inherited from a superclass
+# If you were to define a .to_s in the current class, calling .to_s will evoke that very method
+
+p phone.to_s # => "Miao"
+# => "#<Gadget:0x007fc7be968ad8>" (This would be the default .to_s)
+
+p phone.user # =>
+p laptop.serial # => 
