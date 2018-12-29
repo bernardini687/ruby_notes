9c9134b79363d3963197bb15b68d62a7b2b4b45d
Oscar Bernardini <bernardini687@gmail.com>
2018-12-29 15:46:41 +0100

    Update part-18

--- part-18/classes/gadget.rb
+++ part-18/classes/gadget.rb
@@ -3,13 +3,21 @@ class Gadget
   attr_writer :key, :user
   attr_reader :serial, :user
 
-  def initialize
-    @user = "User #{rand(1..100)}"
-    @key = "****"
+  def initialize(user, key)
+    @user = user
+    @key = key
     @serial = "#{("a".."z").to_a.sample}-#{rand(100..999)}"
   end
 end
 
-phone = Gadget.new
-p phone.user   # => "User 89"
-p phone.serial # => "n-194"
+g1 = Gadget.new("tac", "password")
+p g1.user   # => "tac"
+p g1.serial # => "l-144"
+
+g1.key = "qwerty"
+g1.user = "sophie"
+
+p g1.user   # => "sophie"
+
+g2 = Gadget.new("ping", "1234")
+g3 = Gadget.new("freya", "asdf")
