7c756a1dca4c7286dee9010febf662033110db57
Oscar Bernardini <bernardini687@gmail.com>
2019-01-22 16:52:44 +0100

    Update 2_splat_argument.rb

--- part-10/2_splat_argument.rb
+++ part-10/2_splat_argument.rb
@@ -7,3 +7,11 @@ def sum(*nums) # The * indicates the splat argument
 end
 
 p sum(3, 8, 7, 2) # => 20
+
+def full_name(first, *mids, last)
+  middle_name = String.new
+  mids.each { |name| middle_name += "#{name.capitalize} " }
+  "#{first.capitalize} " + middle_name + "#{last.capitalize}"
+end
+
+p full_name("oscar", "finn", "jim", "bernardini")
